f3df82971df8dbb88339090dfd57f2c161160d49
Dharshak <dharshak.ch97@gmail.com>
2020-10-15 16:49:33 +0530

    settle Balance component

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
 
 const Balance = () => {
+    const { incomeTransactions, expenseTransactions } = useContext(GlobalContext);
+    const incomeAmounts = incomeTransactions.map(
+        it => it.incomeAmount
+    );
+    const expenseAmounts = expenseTransactions.map(
+        et => et.expenseAmount
+    );
+
+    const totalIncome = incomeAmounts
+        .reduce((acc, item) => (acc += item), 0)
+        .toFixed(2);
+
+    const totalExpenses = expenseAmounts
+        .reduce((acc, item) => (acc += item), 0)
+        .toFixed(2);
+    
     return (
         <div className="balance">
             <h2>Your balance:</h2>
-            <h3>$ 0.00</h3>
+            <h3>$ {totalIncome - totalExpenses}</h3>
             <div className="income-expense">
                 <div className="plus">
                     <h3>Income</h3>
-                    <p>+$0.00</p>
+                    <p>+${totalIncome}</p>
                 </div>
                 <div className="minus">
                     <h3>Exxpense</h3>
-                    <p>-$0.00</p>
+                    <p>-${totalExpenses}</p>
                 </div>
             </div>
         </div>

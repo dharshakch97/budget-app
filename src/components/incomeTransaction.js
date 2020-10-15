e70c690e024a8b946f72df6a7f341570fd99c3d6
Dharshak <dharshak.ch97@gmail.com>
2020-10-15 16:39:13 +0530

    add, delete transactions

--- src/components/incomeTransaction.js
+++ src/components/incomeTransaction.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, {useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
 
 const IncomeTransaction = ({ incomeTransactions }) => {
+    const { deleteTransaction } = useContext(GlobalContext);
     return (
         <li className="transaction">
             <span className="transaction-text">
@@ -9,7 +11,7 @@ const IncomeTransaction = ({ incomeTransactions }) => {
             <span className="transaction-amount">$
                 {incomeTransactions.incomeAmount}
             </span>
-            <button className="delete-btn">
+            <button className="delete-btn" onClick={() => deleteTransaction(incomeTransactions.id)}>
                 <i className="fas fa-trash"></i>
             </button>
         </li>
--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -2,27 +2,38 @@ import React, { createContext, useReducer } from 'react';
 import AppReducer from './AppReducer';
 
 const initialState = {
-    incomeTransactions: [
-        { id: 1, incomeText: "Salary", incomeAmount: 20000},
-        { id: 2, incomeText: "Hike", incomeAmount: 5000},
-        { id: 3, incomeText: "Sold items", incomeAmount: 3000}
-    ],
-    expenseTransactions: [
-        { id: 1, expenseText: "Loan", expenseAmount: 5000},
-        { id: 1, expenseText: "House works", expenseAmount: 1000},
-        { id: 1, expenseText: "Travel cost", expenseAmount: 5000}
-    ]
+    incomeTransactions: [],
+    expenseTransactions: []
 };
 
 export const GlobalContext = createContext(initialState);
 
 export const GlobalContextProvider = ({children}) => {
     const [state, dispatch] = useReducer(AppReducer, initialState);
+    const addIncome = (incomeTransaction) => {
+        dispatch({
+            type: "ADD_INCOME",
+            payload: incomeTransaction
+        });
+    }
+    const addExpense = (expenseTransaction) => {
+        dispatch({
+            type: "ADD_EXPENSE",
+            payload: expenseTransaction
+        })
+    }
 
+    const deleteTransaction = (id) => {
+        dispatch({
+            type: "DELETE_TRANSACTION",
+            payload: id
+        })
+    }
     return (
         <GlobalContext.Provider value={{
             incomeTransactions: state.incomeTransactions,
-            expenseTransactions: state.expenseTransactions
+            expenseTransactions: state.expenseTransactions,
+            addIncome, addExpense, deleteTransaction
         }}>
             {children}
         </GlobalContext.Provider>
